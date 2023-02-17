40b8c5a87265b54b50db611286b8c170ec7c2e8e
Kuete Cyrille <cyrillekuete@outlook.com>
2023-02-17 10:08:36 +0100

    Header Component Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,70 @@
 import Navbar from "./components/navbar/Navbar";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Gigs from "./pages/gigs/Gigs";
+import Gig from "./pages/gig/gig";
+import Add from "./pages/addGigs/Add";
+
+import Orders from "./pages/orders/Orders";
+import Messages from "./pages/messages/Message";
+import Message from "./pages/message/Message";
+import MyGigs from "./pages/myGigs/MyGigs";
+import "./App.scss";
 
 function App() {
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/gigs",
+          element: <Gigs />,
+        },
+        {
+          path: "/gig/:id",
+          element: <Gig />,
+        },
+        {
+          path: "/orders",
+          element: <Orders />,
+        },
+        {
+          path: "/mygigs",
+          element: <MyGigs />,
+        },
+        {
+          path: "/add",
+          element: <Add />,
+        },
+        {
+          path: "/messages",
+          element: <Messages />,
+        },
+        {
+          path: "/message:id",
+          element: <Message />,
+        },
+      ],
+    },
+  ]);
   return (
     <div className="App">
-      <Navbar />
-      
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Navbar.scss";
 import { useState, useEffect } from "react";
 
 const Navbar = () => {
+  const { pathname } = useLocation();
   const [active, setActive] = useState(false);
   const [open, setOpen] = useState(false);
   const isActive = () => {
@@ -18,17 +19,17 @@ const Navbar = () => {
   }, []);
 
   const currentUser = {
-    id:1,
+    id: 1,
     username: "John",
-    isSeller: true
-  }
+    isSeller: true,
+  };
   return (
-    <div className={active ? "navbar active" : "navbar"}>
+    <div className={active || pathname !== "/" ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo ">
-          {/* <Link to="/"> */}
-          <span className="text">fiverr</span>
-          {/* </Link> */}
+          <Link to="/" className="link">
+            <span className="text">fiverr</span>
+          </Link>
 
           <span className="dot">.</span>
         </div>
@@ -37,41 +38,76 @@ const Navbar = () => {
           <span>Explore</span>
           <span>English</span>
           <span>Sign in</span>
-        { !currentUser?.isSeller && <span>Become a Seller</span>}
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
           {!currentUser && <button className="">Join</button>}
-          {
-            currentUser && (
-              <div className="user" onClick={() => setOpen(!open)}> 
+          {currentUser && (
+            <div className="user" onClick={() => setOpen(!open)}>
               <img
                 src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
                 alt=""
               />
               <span>{currentUser?.username} </span>
-          {open &&    <div className="options"> 
-              {currentUser?.isSeller &&(
-                <>
-                <span>Gigs </span>
-                <span>Add New Gig </span>
-                </>
+              {open && (
+                <div className="options">
+                  {currentUser?.isSeller && (
+                    <>
+                      <Link className="link" to="/mygigs">
+                        Gigs{" "}
+                      </Link>
+                      <Link className="link" to="/add">
+                        Add New Gig{" "}
+                      </Link>
+                    </>
+                  )}
+                  <Link className="link" to="/orders">
+                    Orders{" "}
+                  </Link>
+                  <Link className="link" to="/messages">
+                    Messages
+                  </Link>
+                  <Link className="link" to="/">
+                    Logout
+                  </Link>
+                </div>
               )}
-              <span>Orders </span>
-              <span>Messages</span>
-              <span>Logout</span>
-              </div>}
-              </div>
-            )
-          }
+            </div>
+          )}
         </div>
       </div>
-      { active && (
+      {(active || pathname !== "/") && (
         <>
-      <hr />
-      <div className="menu">
-        <span>Test</span>
-      </div>
-      </>
-      )
-    }
+          <hr />
+          <div className="menu">
+            <Link className="link menuLink" to="/">
+              Graphics & Design
+            </Link>
+            <Link className="link menuLink" to="/">
+              Video & Animation
+            </Link>
+            <Link className="link menuLink" to="/">
+              writing & Translation
+            </Link>
+            <Link className="link menuLink" to="/">
+              AI Services
+            </Link>
+            <Link className="link menuLink" to="/">
+              Digital Marketing
+            </Link>
+            <Link className="link menuLink" to="/">
+              Music & Audio
+            </Link>
+            <Link className="link menuLink" to="/">
+              Programming & Tech
+            </Link>
+            <Link className="link menuLink" to="/">
+              Business
+            </Link>
+            <Link className="link menuLink" to="/">
+              Lifestyle
+            </Link>
+          </div>
+        </>
+      )}
     </div>
   );
 };
