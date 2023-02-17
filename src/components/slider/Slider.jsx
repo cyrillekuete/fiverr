59e6e382d413f654f69aba01a2b0d0f7479c7207
Kuete Cyrille <cyrillekuete@outlook.com>
2023-02-17 10:47:47 +0100

    Infinite Carousel

---
+++ src/components/slider/Slider.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Slider.scss";
+
+const Slider = () => {
+  return (
+    <div className="slide">
+      <div className="container"></div>
+    </div>
+  );
+};
+
+export default Slider;
