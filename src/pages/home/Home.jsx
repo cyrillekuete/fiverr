1cd93dab97c6cb98c284bc78abcbaf23a55ae7ad
Kuete Cyrille <cyrillekuete@outlook.com>
2023-02-17 10:22:26 +0100

    Trusted By Section Completed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Featured from "../../components/featured/Featured";
+import Trusted from "../../components/trusted/TrustedBy";
 import "./Home.scss";
 
 const Home = () => {
   return (
     <div className="home">
       <Featured />
+      <Trusted />
     </div>
   );
 };
